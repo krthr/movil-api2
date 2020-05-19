0f7a31f7f787dbedb127b86abfe8f91f8d2173b6
krthr <twilson@uninorte.edu.co>
2020-05-18 23:09:41 -0500

    category string

--- src/api/data.cr
+++ src/api/data.cr
@@ -1,41 +1,29 @@
 require "json"
 
 module Data
-  enum Category
-    VERDURA = 1
-    CARNE   = 2
-
-    def to_json(builder)
-      builder.string self
-    end
-  end
-
   class Item
     JSON.mapping(
       name: String,
       price: Float64,
-      category: Category
+      category: String
     )
-
-    def to_s(io)
-      io << "<Item #{name} $#{price}>"
-    end
   end
 
-  DATA = File.open("./src/data.json") { |json|
+  DATA = File.open("./src/data.json") do |json|
     Array(Item).from_json json
-  }
+  end
 
   # Search in the `DATA` array using `q`
   def self.search(q = "", category = "")
-    query = DATA.select { |item|
-      item.name
-        .lstrip
-        .downcase
-        .includes? q.lstrip.downcase
-    }
+    query = DATA.select do |item|
+      categoryQuery = category == "" ? true : category == item.category
 
-    return query.select { |item| category == item.category.to_s } if category != ""
+      categoryQuery &&
+        item.name
+          .lstrip
+          .downcase
+          .includes? q.lstrip.downcase
+    end
 
     query
   end
